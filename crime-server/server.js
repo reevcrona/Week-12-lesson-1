0c9e83d65f6369aa1b2d4a60e3f5047220ddcbda
Jreev <jacob.reevcrona@yahoo.com>
2025-04-07 14:21:32 +0200

    successfully setup all the routes, next up test using the debugger and improve error handling

--- crime-server/server.js
+++ crime-server/server.js
@@ -7,8 +7,63 @@ const app = express();
 
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello");
+app.get("/crimes", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://brottsplatskartan.se/api/events/?location=helsingborg&limit=5"
+    );
+    const crimes = response.data.data;
+
+    res.status(200).json(crimes);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/crimes/locations", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://brottsplatskartan.se/api/events/?location=helsingborg&limit=5"
+    );
+
+    const headlineData = {
+      Headlines: response.data.data.map((crime) => crime.headline),
+    };
+
+    res.status(200).json(headlineData);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/crimes/search", async (req, res) => {
+  try {
+    const city = req.query.city;
+
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/events/?location=${city}&limit=5`
+    );
+
+    const crimes = response.data.data;
+
+    res.status(200).json(crimes);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get("/crimes/latest", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://brottsplatskartan.se/api/events/?location=helsingborg&limit=1`
+    );
+
+    const latestCrime = response.data.data[0];
+
+    res.status(200).json(latestCrime);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 app.listen(port, () => {
