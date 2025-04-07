d3399dd63bec7cdb8df47c9e6c38c6610a858034
Jreev <jacob.reevcrona@yahoo.com>
2025-04-07 13:29:28 +0200

    forgot to add the crime-server folder

--- crime-server/server.js
+++ crime-server/server.js
@@ -7,6 +7,10 @@ const app = express();
 
 const port = 3000;
 
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
 app.listen(port, () => {
   console.log(`Server is up and running on port ${port}`);
 });
